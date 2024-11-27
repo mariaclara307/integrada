8461872c4e7b1e59026c871f77806204086606cb
maria <mariarecco.30@gmail.com>
2024-11-27 00:36:28 -0300

    js
    criado

---
+++ documentação.js
@@ -0,0 +1,13 @@
+function showSection(section) {
+    // Esconde todas as seções
+    document.querySelectorAll('section').forEach(function(sec) {
+        sec.classList.add('d-none');
+    });
+    // Exibe a seção selecionada
+    document.getElementById(section).classList.remove('d-none');
+}
+function showTask(section, subject) {
+    // Esconde todas as informações de tarefas
+    document.querySelectorAll(`#task-info-${section}`).forEach(function(task) {
+        task.classList.add('d-none');
+    })};
